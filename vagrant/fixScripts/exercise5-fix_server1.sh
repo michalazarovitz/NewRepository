832742ecf5afde0d26a58be6d570fa1f4ae95674
michalazarovitz <55147076+michalazarovitz@users.noreply.github.com>
2019-09-15 16:32:00 +0300

    Update exercise5-fix_server1.sh

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-#add fix to exercise5-server1 here
+#ssh passwordless to server2
+ssh-keygen -t rsa
+ssh server2 mkdir -p .ssh
+cat .ssh/id_rsa.pub | ssh server2 'cat >> .ssh/authorized_keys'
+ssh server2 "chmod 700.ssh; chmod 640.ssh/authorized_keys"
+ssh server2
